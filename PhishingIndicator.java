ccfe344706aaf3c96008fd3e002e33d433f266b6
StefanJosevski <stefan.josevski04@gmail.com>
2026-02-20 14:47:23 -0500

    Create PhishingIndicator class with properties
    This class represents a phishing indicator with a name, description, and weight.

---
+++ PhishingIndicator.java
@@ -0,0 +1,31 @@
+package com.securescan.model;
+
+public class PhishingIndicator {
+
+    private final String name;
+    private final String description;
+    private final int weight;
+
+    public PhishingIndicator(String name, String description, int weight) {
+        this.name = name;
+        this.description = description;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + weight + "): " + description;
+    }
+}
